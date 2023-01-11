84cd496b8fbed3a241102756cce23c06e38ea44c
YPrawiroCode <yusufprawiro14@gmail.com>
2023-01-11 14:24:30 +0700

    update

--- src/mqtt/mqtt.controller.ts
+++ src/mqtt/mqtt.controller.ts
@@ -3,9 +3,15 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('math')
 export class MqttController {
-  @MessagePattern('notification_channel')
-  getNotifications(@Payload() data) {
+  @MessagePattern('notification_channel_a')
+  getNotificationsA(@Payload() data) {
     console.log(data);
-    return `I Got Message From Client: ${data}`;
+    return `I Got Message From Client with channel a: ${data}`;
+  }
+
+  @MessagePattern('notification_channel_b')
+  getNotificationsB(@Payload() data) {
+    console.log(data);
+    return `I Got Message From Client with channel b: ${data}`;
   }
 }
