24860d30cfc19094d6d66f3ca215f33b7d727ad2
YPrawiroCode <yusufprawiro14@gmail.com>
2023-01-11 14:25:39 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get /* Inject */ } from '@nestjs/common';
+// import { ClientProxy } from '@nestjs/microservices';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(@Inject('MQTT_SERVICE') private client: ClientProxy) {}
+  constructor(
+    // @Inject('MQTT_SERVICE') private client: ClientProxy,
+    private readonly appService: AppService,
+  ) {}
   @Get('notifications')
-  getNotifications() {
-    return this.client.send(
-      'notification_channel',
-      "It's a Message From Client",
-    );
+  getNotif(@Body() body: any) {
+    return this.appService.getNotifications(body);
+  }
+  // getNotifications() {
+
+  // return this.client.send(
+  //   'notification_channel',
+  //   "It's a Message From Client",
+  // );
+  // }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MQTT_SERVICE') private client: ClientProxy) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getNotifications(body) {
+    console.log(
+      '🚀 ~ file: app.service.ts:12 ~ AppService ~ getNotifications ~ body',
+      body.test,
+    );
+    let channel = '';
+    let message = '';
+    switch (body.test) {
+      case 'chanel1':
+        channel = 'notification_channel_a';
+        message = "It's a Message From Client with channel a";
+        break;
+      case 'chanel2':
+        channel = 'notification_channel_b';
+        message = "It's a Message From Client with channel b";
+        break;
+    }
+    return this.client.send(channel, message);
   }
 }
